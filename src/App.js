1ad6de62e5e32db58167710dd4c9a7b28c377716
Edyta Wojdala <edytawoj095@gmail.com>
2020-08-31 15:39:12 +0200

    Add NavLinks

--- src/App.js
+++ src/App.js
@@ -9,20 +9,20 @@ import Kitchen from './components/views/Kitchen/Kitchen';
 
 function App() {
   return (
-    <MainLayout>
-      <BrowserRouter basename={'/panel'}>
-        <MainLayout>
-          <Switch>
-            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
-            <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
-            <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={TablesStatus} />
-            <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
-            <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
+    
+    <BrowserRouter basename={'/panel'}>
+      <MainLayout>
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
+          <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
+          <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={TablesStatus} />
+          <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
+          <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
             
-          </Switch>
-        </MainLayout>
-      </BrowserRouter>
-    </MainLayout>
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
+    
   );
 }
 
