5fee58661539f5feb57961bcb847de9e6a25d82e
Edyta Wojdala <edytawoj095@gmail.com>
2020-10-02 21:10:22 +0200

    Add material UI

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 import styles from './Login.module.scss';
+import { Paper } from '@material-ui/core';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
 
 const Login = () => (
-  <div className={styles.component}>
-    <h2>Login view</h2>
-  </div>
+  <Paper className={styles.component}>
+    <TableHead>Login view
+      <TableRow>
+        <TableCell>ID number</TableCell>
+        <TableCell><input></input></TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Pasword</TableCell>
+        <TableCell><input></input></TableCell> 
+      </TableRow>
+    </TableHead>
+    <Button>Log in</Button>
+  </Paper>
 );
 
 export default Login;
--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -2,15 +2,91 @@ import React from 'react';
 import styles from './Tables.module.scss';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import Button from '@material-ui/core/Button';
+
+const demoContent = [
+  {id: '10:00 ', status: 'reservation', status2: null},
+  {id: '10:30 ', status: 'event', order: null},
+];
+
+const renderActions = status => {
+  switch (status) {
+    case 'free':
+      return (
+        <>
+          <Button>reservation</Button>
+          <Button>new order</Button>
+        </>
+      );
+    case 'thinking':
+      return (
+        <Button>new order</Button>
+      );
+    case 'ordered':
+      return (
+        <Button>prepared</Button>
+      );
+    case 'prepared':
+      return (
+        <Button>delivered</Button>
+      );
+    case 'delivered':
+      return (
+        <Button>paid</Button>
+      );
+    case 'paid':
+      return (
+        <Button>free</Button>
+      );
+    default:
+      return null;
+  }
+};
 
 const Tables = () => (
-  <div className={styles.component}>
-    <h2>Tables view</h2>
-    <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`}>NEW</Link><span> </span>
-    <Link to={`${process.env.PUBLIC_URL}/tables/booking/123`}>ID</Link><span> </span>
-    <Link to={`${process.env.PUBLIC_URL}/tables/events/new`}>EVENTS_NEW</Link><span> </span>
-    <Link to={`${process.env.PUBLIC_URL}/tables/events/eventID`}>ID</Link><span> </span>
-  </div>
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Time</TableCell>
+          <TableCell>table 1</TableCell>
+          <TableCell>table 2</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {demoContent.map(row => (
+          <TableRow key={row.id}>
+            <TableCell component="th" scope="row">
+              {row.id}
+            </TableCell>
+            <TableCell>
+              {row.status}
+            </TableCell>
+            <TableCell>
+              {row.order && (
+                <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                  {row.order}
+                </Button>
+              )}
+            </TableCell>
+            <TableCell>
+              {renderActions(row.status)}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`}>NEW</Link><span> </span>
+      <Link to={`${process.env.PUBLIC_URL}/tables/booking/123`}>ID</Link><span> </span>
+      <Link to={`${process.env.PUBLIC_URL}/tables/events/new`}>EVENTS_NEW</Link><span> </span>
+      <Link to={`${process.env.PUBLIC_URL}/tables/events/eventID`}>ID</Link><span> </span>
+    </Table>
+  </Paper>
 );
 
 Tables.propTypes = {
