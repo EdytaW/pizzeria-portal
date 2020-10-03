aa3acc9bbb8c6ce136ebc627459bf67238d16c92
Edyta Wojdala <edytawoj095@gmail.com>
2020-10-03 16:42:10 +0200

    Add booking view

--- src/components/views/Booking/Booking.js
+++ src/components/views/Booking/Booking.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import styles from './Booking.module.scss';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { TableBody } from '@material-ui/core';
 
+const democontent = [
+  {table: '1', time: '10:30'},
+  {table: '2', time: '10:00'},
+];
 const Booking = (props) => (
-  <div className={styles.component}>
+  <Paper className={styles.component}>
     <h2>Booking view</h2>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Table</TableCell>
+          <TableCell>Time</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {democontent.map(row => (
+          <TableRow key={row.table}>
+            <TableCell component="th" scope="row">
+              {row.table}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {row.time}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
     {props.match.params.id === 'new' ? 
       (<> 
     Create New 
       </>) :
       (<> ID: {props.match.params.id} 
       </>)};
-  </div>
+  </Paper>
 );
 
 Booking.propTypes = {
